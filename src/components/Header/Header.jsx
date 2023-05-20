962b982a619e4d579b136b16a16f7adbecb8926b
MuhdHanish <muhammedhanish11@gmail.com>
2023-05-20 16:24:15 +0530

    cart count added in head but there is some small issues

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
-import "./Header.css"
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import "./Header.css";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const {cartList} = useSelector((state)=>state.cart)
   return (
     <header>
       <div className="container">
         <h1>Shopping Cart</h1>
-        <div className="icon-container">
-          <FontAwesomeIcon icon={faCartShopping} size="2x"/>
+        <div className="right-section">
+          <div className="icon-container">
+            <div className="cart-count-header">{cartList?.length}</div>
+            <FontAwesomeIcon icon={faCartShopping} size="2x" />
+          </div>
         </div>
       </div>
     </header>
