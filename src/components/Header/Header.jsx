c19bf3f85ac38cddf0d65c295723c08a1e962a81
MuhdHanish <muhammedhanish11@gmail.com>
2023-05-20 10:57:16 +0530

    Add some basic styles and add header and cart icon and create a api file and some datas then data displayed in product list

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import "./Header.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+
 
 const Header = () => {
   return (
     <header>
-     
+      <div className="container">
+        <h1>Shopping Cart</h1>
+        <FontAwesomeIcon icon={faCartShopping} shake size='2xl' />
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import "./ProductList.css"
+import products from '../../api/products.json'
 
 const ProductList = () => {
   return (
-    <div>ProductList</div>
+    <section className='container'>
+     {
+      products.map((product,key)=>(
+      <div className="main-container">
+        <img src={product?.image} alt="" />
+        <h3>{product?.title}</h3>
+        
+      </div>
+      ))
+     }
+    </section>
   )
 }
 
